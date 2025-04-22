93a9af9b6528dd39da3ae96ef0b6c8fb38d97c47
Danial <danialamg64@gmail.com>
2025-04-22 23:31:32 +0330

    Getting to know control characters

---
+++ Chapter1:Introduction and Input/Output/pr02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+//Getting to know control characters
+int main(){
+    std::cout<<"this is my secend program in C++";
+    std::cout<<"im writing the program in vscode"<<"\n";
+    std::cout<<"i love C++ "<<"\n";
+    std::cout<<"hi from \t\nC++";
+}
