1d7771f1104238a6529fcfbf1c53b6faf8d6adc0
Danial <danialamg64@gmail.com>
2025-04-22 23:36:29 +0330

    Getting to know control characters

--- Chapter1:Introduction and Input/Output/pr02.cpp
+++ Chapter1:Introduction and Input/Output/pr02.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 //Getting to know control characters
+// \n Go to the next line
+// \t A fever is going away
+// \a create sound
+// \b Deletes the character before it.
 int main(){
     std::cout<<"this is my secend program in C++";
     std::cout<<"im writing the program in vscode"<<"\n";
-    std::cout<<"i love C++ "<<"\n";
+    std::cout<<"i love C++ "<<"\a";
     std::cout<<"hi from \t\nC++";
 }
